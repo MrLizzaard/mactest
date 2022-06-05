32bc55f25b703fa623d3a305c117fe80bf908847
Do Hyeong Kim <dhk21512@gmail.com>
2022-06-05 18:57:55 +0900

    sass

--- src/app/About/About.js
+++ src/app/About/About.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function About(props) {
-  return <div>About</div>;
+  return <div>MrLizard가 만든 게시판입니다</div>;
 }
 
 export default About;
--- src/app/Main/Main.js
+++ src/app/Main/Main.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Main(props) {
-  return <div>Main</div>;
+  return <div>메인페이지입니다.</div>;
 }
 
 export default Main;
